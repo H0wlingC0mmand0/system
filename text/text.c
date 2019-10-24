10f2713d405c428dae86d80119b108efee829979
H0wlingC0mmand0 <48954039+H0wlingC0mmand0@users.noreply.github.com>
2019-10-24 09:43:40 +0530

    Update text.c

--- text/text.c
+++ text/text.c
@@ -1,148 +1,149 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<windows.h>
-void newf();
-void view();
-void saveas();
-void modify();
+#include<process.h>
+int i,j,ch;
+char fn[20],e,c;
+FILE *fp1,*fp2,*fp;
+void Create();
+void Append();
+void Copy();
+void Delete();
+void Display();
 void main()
 {
-int op;
-do
-{
-printf("\nMENU\n");
-printf("1.New\n");
-printf("2.Open\n");
-printf("3.Save As\n");
-printf("4.Modify\n");
-printf("5.Exit\n");
-printf("Enter u'r choice: ");
-scanf("%d",&op);
-switch(op)
+do {
+printf("\n\t\t***** TEXT EDITOR *****");
+printf("\n\n\tMENU:\n\t\n");
+printf("\n\t1.CREATE\n\t2.DISPLAY\n\t3.APPEND\n\t4.COPY\n\t5.DELETE\n\t6.EXIT\n");
+printf("\n\tEnter your choice: ");
+scanf("%d",&ch);
+switch(ch)
 {
 case 1:
-
-newf();
+Create();
 break;
 case 2:
-view();
+Display();
 break;
 case 3:
-saveas();
+Append();
 break;
 case 4:
-modify();
+Copy();
 break;
 case 5:
+Delete();
+break;
+case 6:
 exit(0);
-default:
-printf("\nWrong choice!!!");
+}
+}while(1);
+}
+void Create()
+{
+fp1=fopen("temp.txt","w");
+printf("\n\tEnter the text and press '.' to save\n\n\t");
+while(1)
+{
+c=getchar();
+fputc(c,fp1);
+if(c == '.')
+{
+fclose(fp1);
 break;
 }
-}while(op!=5);
+}}
+ 
+void Display()
+{
+printf("\n\tEnter the file name: ");
+scanf("%s",fn);
+fp1=fopen(fn,"r");
+if(fp1==NULL)
+{
+printf("\n\tFile not found!");
+goto end1;
 }
-void newf()
+while(!feof(fp1))
 {
-FILE *f;
-char fname[20],c;
-printf("\nEnter the Filename: ");
-scanf("%s",fname);
-printf("\nType the content and CTRL+Z to terminate:\n");
-f=fopen(fname,"w");
-rewind(f);
-while((c=getchar())!=EOF)
-{
-putc(c,f);
-}
-fclose(f);
-}
-void view()
-{
-FILE *f;
-char fname[20],c;
-printf("\nEnter the name of the file:");
-scanf("%s",&fname);
-if(SearchPath(fname))
-{
-f=fopen(fname,"r");
-while((c=getc(f))!=EOF)
-{
+c=getc(fp1);
 printf("%c",c);
 }
+end1:
+fclose(fp1);
+printf("\n\n\tPress any key to continue\n");
+}
+void Copy()
+{
+printf("\n\tEnter the new filenameto copy:  ");
+scanf("%s",fn);
+fp1=fopen("temp.txt","r");
+fp2=fopen(fn,"w");
+while(!feof(fp1))
+{
+c=getc(fp1);
+putc(c,fp2);
+}
+fclose(fp2);
+}
+ 
+void Delete()
+{
+printf("\n\tEnter the file name: ");
+scanf("%s",fn);
+fp1=fopen(fn,"r");
+if(fp1==NULL)
+{
+printf("\n\tFile not found!");
+goto end2;
+}
+fclose(fp1);
+if(remove(fn)==0)
+{
+printf("\n\n\tFile has been deleted successfully!");
+goto end2;
 }
 else
-printf("\nThe file %s does not exist",fname);
-fclose(f);
-
-}
-void saveas()
-{
-FILE *f1,*f2;
-char c,sou[20],des[20];
-printf("\nEnter the Source file name: ");
-scanf("%s",sou);
-if(SearchPath(sou))
-{
-printf("Enter the Destination file name: ");
-scanf("%s",des);
-f1=fopen(sou,"r");
-f2=fopen(des,"w");
-while((c=getc(f1))!=EOF)
-{
-putc(c,f2);
-}
-fclose(f1);
-fclose(f2);
-}
-else
-printf("\nFile does not exist");
+printf("\n\tError!\n");
+end2: printf("\n\n\tPress any key to continue\n");
 getch();
 }
-void modify()
+ 
+void Append()
 {
-int ch1;
-FILE *f1;
-char c,*word,*sent,fname[20];
-printf("Enter the filename to be modified: ");
-scanf("%s",fname);
-if(SearchPath(fname))
+printf("\n\tEnter the file name: ");
+scanf("%s",fn);
+fp1=fopen(fn,"r");
+if(fp1==NULL)
 {
-printf("\n1.Character");
-printf("\n2.Word");
-printf("\n3.Sentence");
-printf("\nEnter U'r choice: ");
-scanf("%d",&ch1);
-if(ch1==1)
-{
-f1=fopen(fname,"a+");
-fseek(f1, 0L, SEEK_END);
-printf("Enter the character and CTRL+Z to exit:\n ");
-while((c=getchar())!=EOF)
-{
-putc(c,f1);
+printf("\n\tFile not found!");
+fclose(fp1);
+goto end3;
 }
-}
-else if(ch1==2)
+while(!feof(fp1))
 {
-printf("Enter the word: ");
-
-scanf("%s",word);
-f1=fopen(fname,"a+");
-fseek(f1, 0L, SEEK_END);
-fputs(word,f1);
+c=getc(fp1);
+printf("%c",c);
+}
+fclose(fp1);
+printf("\n\tType the text and press Ctrl+S to append.\n");
+fp1=fopen(fn,"a");
+while(1)
+{
+c=getch();
+if(c==19)
+goto end3;
+if(c==13)
+{
+c='\n';
+printf("\n\t");
+fputc(c,fp1);
 }
 else
 {
-printf("Enter the sentence and CTRL+Z to exit: ");
-f1=fopen(fname,"a+");
-fseek(f1, 0L, SEEK_END);
-while((c=getchar())!=EOF)
-{
-putc(c,f1);
+printf("%c",c);
+fputc(c,fp1);
 }
 }
-}
-else
-printf("\nFilename does not exist");
+end3: fclose(fp1);
+ 
 }
